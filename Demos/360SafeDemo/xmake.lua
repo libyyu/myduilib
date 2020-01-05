e864090e91d5f29a5e78c807efcf955ac2201b68
libyyu <libyyu@qq.com>
2020-01-05 22:54:31 +0800

    lua bin

---
+++ Demos/360SafeDemo/xmake.lua
@@ -0,0 +1,27 @@
+target("360SafeDemo")
+    -- set kind
+    set_kind("shared")
+    -- set warning all and disable error
+    set_warnings("all")
+    -- set language: c99, c++11
+    set_languages("cxx11")
+    --添加链接库
+    add_deps("DuiLib")
+    add_links("DuiLib")
+    -- add includes directory
+    add_includedirs(
+        "../../DuiLib"
+    )
+
+    add_files("*.cpp")
+
+    add_defines("BUILD_AS_DLL", "_AFXDLL", "_USEIMM", "PLUGIN_MODULE")
+    add_rules("app.dll")
+
+    --添加头文件
+    add_options("genproj")
+    if has_config("genproj") then
+        add_headerfiles("*.h", "*.hpp")
+    end
+
+    set_values("vs.folder", "demos")
